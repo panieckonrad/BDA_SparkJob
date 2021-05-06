dc5cfcc837d07ae73c6e859ada503cb5de34be29
Łukasz Dębski <lukasz.debski@kainos.com>
2021-05-06 07:13:10 +0000

    Merged PR 16: Adding CLI scripts
    Adding CLI scripts for creating database and container inside Cosmo DB account
    
    Related work items: #29

--- cli-cmd.sh
+++ cli-cmd.sh
@@ -1,6 +1,11 @@
 # create test CosmosDB account
 az cosmosdb create --name forextestdb --resource-group BigDataAcademyMay2021
 
+# create database for stream analytics inside CosmoDB account
+az cosmosdb sql database create --name asa --account-name forextestdb --resource-group BigDataAcademyMay2021
+# create container for stream analytics inside CosmoDB database
+az cosmosdb sql container create --account-name forextestdb --database-name asa --name output --partition-key-path /Instrument --resource-group BigDataAcademyMay2021
+
 # create test Azure Storage Table
 az storage table create --name forextesttbl --account-name forexstoragebda
 # create read policy on Azure Storage Table
